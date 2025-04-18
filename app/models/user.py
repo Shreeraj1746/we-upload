2121bc9672391cbb347a7ec0e2cb6a01ef72f4d3
Shreeraj Dabholkar <shreeraj.dabholkar@gmail.com>
2025-04-18 23:47:13 +0530

    fix: mypy errors

--- app/models/user.py
+++ app/models/user.py
@@ -31,8 +31,8 @@ class User(Base):
     email = Column(String(255), unique=True, index=True, nullable=False)
     full_name = Column(String(255), nullable=True)
     hashed_password = Column(String(255), nullable=False)
-    is_active = Column(Boolean(), default=True)
-    is_superuser = Column(Boolean(), default=False)
+    is_active = Column(Boolean(), default=True)  # type: ignore
+    is_superuser = Column(Boolean(), default=False)  # type: ignore
     created_at = Column(DateTime, default=func.now(), nullable=False)
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
 
