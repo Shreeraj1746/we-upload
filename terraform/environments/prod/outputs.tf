7f0826050d762898d8b712cd687e22770bebcbc2
Shreeraj Dabholkar <shreeraj.dabholkar@gmail.com>
2025-04-25 09:29:53 +0530

    fix: use correct s3 vars for prod config

--- terraform/environments/prod/outputs.tf
+++ terraform/environments/prod/outputs.tf
@@ -15,12 +15,12 @@ output "private_subnet_ids" {
 
 output "s3_bucket_name" {
   description = "Name of the S3 bucket"
-  value       = module.s3.bucket_name
+  value       = module.s3.s3_bucket_name
 }
 
 output "s3_bucket_arn" {
   description = "ARN of the S3 bucket"
-  value       = module.s3.bucket_arn
+  value       = module.s3.s3_bucket_arn
 }
 
 output "ec2_instance_id" {
